fecc9ce6b3e79cc2b38efe02056bf7a42b3f9208
Gustavo Alves <111063425+Gustavo-Alves-Silva@users.noreply.github.com>
2022-12-07 21:02:53 -0300

    Update Script Banco de Dados.sql

--- MySQL DB/DB SQL Scripts/Script Banco de Dados.sql
+++ MySQL DB/DB SQL Scripts/Script Banco de Dados.sql
@@ -19,6 +19,9 @@
 -- Table structure for table `controle_entrada`
 --
 
+Create database mydb;
+use mydb;
+
 DROP TABLE IF EXISTS `controle_entrada`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
